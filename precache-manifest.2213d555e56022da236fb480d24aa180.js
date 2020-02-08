74652028a166cc81a8a5541e542f931518ffb92b
Kshitiz Srivastava <ksh1998@gmail.com>
2020-02-08 11:11:02 +0530

    Updates

---
+++ precache-manifest.2213d555e56022da236fb480d24aa180.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf0fac78b9fc36c1c4288cd8d873c149",
+    "url": "/piratetube/index.html"
+  },
+  {
+    "revision": "c2e79571b740160a95d0",
+    "url": "/piratetube/static/css/main.1100a2a6.chunk.css"
+  },
+  {
+    "revision": "22bff91d167d58745bf5",
+    "url": "/piratetube/static/js/2.5e2d1582.chunk.js"
+  },
+  {
+    "revision": "98061d712500f07672e3581cfdcd5a24",
+    "url": "/piratetube/static/js/2.5e2d1582.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2e79571b740160a95d0",
+    "url": "/piratetube/static/js/main.27b68e8e.chunk.js"
+  },
+  {
+    "revision": "2c5a723276d5ad6f0cfd",
+    "url": "/piratetube/static/js/runtime-main.a278491e.js"
+  }
+]);
